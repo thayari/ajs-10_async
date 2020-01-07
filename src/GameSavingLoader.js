31bd255629c3cf7d7da8404aa7d581fe66e4406e
thayari <alex.silvanovich@gmail.com>
2020-01-08 02:05:42 +0300

    async

--- src/GameSavingLoader.js
+++ src/GameSavingLoader.js
@@ -1,19 +1,20 @@
 import read from './reader';
 import json from './parser';
 
+async function gameSaving(value) {
+  return JSON.parse(value);
+}
+
 export default class GameSavingLoader {
+  // eslint-disable-next-line class-methods-use-this
   async load() {
     try {
       const response = await read();
       const value = await json(response);
       const saving = await gameSaving(value);
-      console.log(saving);
-    } catch(e) {
-      console.log(e)
-    } 
+      return saving;
+    } catch (e) {
+      console.log(e);
+    }
   }
-}
-
-async function gameSaving(value) {
-  return JSON.parse(value);
 }
